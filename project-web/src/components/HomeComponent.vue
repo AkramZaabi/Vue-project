4ee5b50a7c91c0e07ae9974c3e7fce24792d73df
Akram Zaabi <akramzaabi7@gmail.com>
2023-11-24 19:35:37 +0100

    fixed-block-products

--- project-web/src/components/HomeComponent.vue
+++ project-web/src/components/HomeComponent.vue
@@ -3,9 +3,7 @@
     <div id="text-content">
       <h1>Trendify</h1>
       <h1>Mode</h1>
-      <h2 class="mt-4">
-        Success Starts with <b style="font-weight: bold">a great product!</b>
-      </h2>
+      <h2 class="mt-4">Success Starts with a great product!</h2>
       <button class="shop-now-button mt-3">
         <span>Shop Now</span>
         <img src="../assets/icons8-e-commerce-66.png" alt="Shop Now!" />
@@ -24,22 +22,64 @@
     <div class="container mt-5">
       <h3>View all!</h3>
     </div>
-    <div class="container" id="prodcut-img">
-      <div class="card shadow p-3 mb-5 bg-body-tertiary rounded cloth">
-        <img src="../assets/t-shirt.png" />
-        <span class="mt-2 span-product">80 TND</span>
-      </div>
-      <div class="card shadow p-3 mb-5 bg-body-tertiary rounded cloth">
-        <img src="../assets/t-shirt.png" />
-        <span class="mt-2 span-product">80 TND</span>
-      </div>
-      <div class="card shadow p-3 mb-5 bg-body-tertiary rounded cloth">
-        <img src="../assets/t-shirt.png" />
-        <span class="mt-2 span-product">80 TND</span>
-      </div>
-      <div class="card shadow p-3 mb-5 bg-body-tertiary rounded cloth">
-        <img src="../assets/t-shirt.png" />
-        <span class="mt-2 span-product">80 TND</span>
+    <div class="container shadow p-3 mb-5 bg-body rounded" id="cloathing">
+      <div class="row row-cols-1 row-cols-5 g-4 mb-5 mt-5 clothes-card">
+        <div class="col">
+          <div class="card" style="width: 12rem">
+            <img src="../assets/t-shirt.png" />
+            <div class="card-body text-center">
+              <h5 class="card-title">TSHIRT</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">80 TND</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 12rem">
+            <img src="../assets/t-shirt.png" />
+            <div class="card-body text-center">
+              <h5 class="card-title">TSHIRT</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">80 TND</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 12rem">
+            <img src="../assets/t-shirt.png" />
+            <div class="card-body text-center">
+              <h5 class="card-title">TSHIRT</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">80 TND</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 12rem">
+            <img src="../assets/t-shirt.png" />
+            <div class="card-body text-center">
+              <h5 class="card-title">TSHIRT</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">80 TND</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card" style="width: 12rem">
+            <img src="../assets/t-shirt.png" />
+            <div class="card-body text-center">
+              <h5 class="card-title">TSHIRT</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">80 TND</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -59,37 +99,49 @@ h3 {
   font-size: 15px;
   cursor: pointer;
 }
-h3:hover {
+
+.card-text:hover {
   color: #0c9cda;
   font-weight: bold;
   transition: 0.3s;
 }
+p {
+  color: #007bff;
+  border: 1px solid #007bff;
+  border-radius: 15px;
+}
 button {
   width: 80%;
 }
+
 button:hover {
   background-color: white !important;
   color: #0c9cda;
   border: 1px solid #0c9cda;
 }
+
 button span {
   font-weight: 500;
 }
+
 #text-content {
   width: 50%;
   display: flex;
   flex-direction: column;
   align-content: space-around;
 }
+
 h1 {
   color: #0c9cda;
   font-weight: bold;
 }
+
 h2 {
   color: #707070;
   width: 50%;
   font-weight: 100;
 }
+
 .container {
   align-self: center;
   place-items: center;
@@ -100,13 +152,15 @@ h2 {
   text-align: left;
   height: 100% !important;
 }
-.span-product:hover {
+
+.card-text:hover {
   color: white;
   border: 1px solid white;
   background-color: #0c9cda;
   cursor: pointer;
   transition: 0.5s;
 }
+
 .span-product {
   font-size: 15px;
 }
@@ -116,10 +170,12 @@ h2 {
   flex-direction: column;
   flex-wrap: wrap;
 }
+
 .cloth span {
   justify-self: center;
   color: #0c9cda;
 }
+
 .cloth span {
   text-align: center;
   border: 1px solid #0c9cda;
@@ -132,6 +188,7 @@ h2 {
   border-radius: 5px;
   border: 1px solid #0c9cda;
 }
+
 .container img {
   height: 70%;
 }
@@ -144,15 +201,18 @@ h2 {
   width: 40%;
   height: 50%;
 }
+
 /* New CSS styles */
 #prodcut-img {
   display: flex;
   justify-content: space-between;
   width: 100% !important;
 }
+
 #prodcut-img .card {
   width: 20% !important;
 }
+
 .success-starts img {
   width: 250px;
 }
@@ -164,6 +224,7 @@ h2 {
   flex-direction: column;
   width: 50%;
 }
+
 .shop-now-button {
   width: 140px;
   height: 30px;
@@ -175,9 +236,11 @@ h2 {
   margin-bottom: 50px;
   font-weight: bold;
 }
+
 .success-starts {
   display: flex;
 }
+
 .shop-now-button img {
   width: 25px;
   height: 25px;
@@ -186,5 +249,10 @@ h2 {
 .success-starts p {
   flex: 1;
   margin-left: 10px;
+}
+@media (max-width: 1250px) {
+  .clothes-card {
+    display: block;
+  }
 }
 </style>
