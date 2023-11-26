ace27d758385a663e12dcb4fbc03adff5bebccdd
Bady <grami.medaziz00@gmail.com>
2023-11-26 13:17:27 +0100

    Merged the two branches and fixed ProductListComponent design

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="container mt-5">
     <div id="text-content">
-      <h1>Modista</h1>
+      <h1>Trendify</h1>
       <h1>Mode</h1>
-      <h2 class="mt-4">
-        Success Starts with <b style="font-weight: bold">a great product!</b>
-      </h2>
-      <button class="shop-now-button mt-3 btn btn-primary">
+      <h2 class="mt-4">Success Starts with a great product!</h2>
+      <button class="shop-now-button mt-3">
         <span>Shop Now</span>
         <img src="../assets/icons8-e-commerce-66.png" alt="Shop Now!" />
       </button>
@@ -21,22 +19,102 @@
   </div>
   <div class="mt-5 clothes">
     <h1>Recommended for you !</h1>
-    <div class="container" id="prodcut-img">
-      <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
-        <img src="../assets/t-shirt.png" />
+    <div class="container mt-5">
+      <router-link class="nav-link" id="h3" to="/product">
+        <h3 style="font-size: 18px">View all!</h3></router-link
+      >
+    </div>
+    <div class="container shadow p-3 mb-5 bg-body rounded" id="cloathing">
+      <div class="row row-cols-1 row-cols-5 g-4 mb-5 mt-5 clothes-card">
+        <div class="col">
+          <div class="card" style="width: 12rem">
+            <img src="../assets/t-shirt.png" />
+            <div class="card-body text-center">
+              <h5 class="card-title">TSHIRT</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">80 TND</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 12rem">
+            <img src="../assets/t-shirt.png" />
+            <div class="card-body text-center">
+              <h5 class="card-title">TSHIRT</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">80 TND</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 12rem">
+            <img src="../assets/t-shirt.png" />
+            <div class="card-body text-center">
+              <h5 class="card-title">TSHIRT</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">80 TND</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 12rem">
+            <img src="../assets/t-shirt.png" />
+            <div class="card-body text-center">
+              <h5 class="card-title">TSHIRT</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">80 TND</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card" style="width: 12rem">
+            <img src="../assets/t-shirt.png" />
+            <div class="card-body text-center">
+              <h5 class="card-title">TSHIRT</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">80 TND</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
-        <img src="../assets/t-shirt.png" />
-      </div>
-      <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
-        <img src="../assets/t-shirt.png" />
-      </div>
-      <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
-        <img src="../assets/t-shirt.png" />
-      </div>
-      <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
-        <img src="../assets/t-shirt.png" />
-      </div>
+    </div>
+  </div>
+  <div class="container mt-5" id="services">
+    <div style="width: 18rem" class="card-services">
+      <img src="../assets/Free shipping-bro.svg" />
+      <span class="text-center mt-2 mb-3">Free Shipping</span>
+      <p class="text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla maiores
+        omnis suscipit officiis deleniti, beatae, commodi recusandae corrupti
+        rerum animi magni perferendis repellat itaque in velit saepe quis eaque
+        voluptatum?
+      </p>
+    </div>
+    <div style="width: 18rem" class="card-services">
+      <img src="../assets/Product quality-amico.svg" />
+      <span class="text-center mt-2 mb-3">Quality Product</span>
+      <p class="text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla maiores
+        omnis suscipit officiis deleniti, beatae, commodi recusandae corrupti
+        rerum animi magni perferendis repellat itaque in velit saepe quis eaque
+        voluptatum?
+      </p>
+    </div>
+    <div style="width: 18rem" class="card-services">
+      <img src="../assets/Coronavirus Delivery Preventions-bro.svg" />
+      <span class="text-center mt-2 mb-3">Safe Measurements</span>
+      <p class="text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla maiores
+        omnis suscipit officiis deleniti, beatae, commodi recusandae corrupti
+        rerum animi magni perferendis repellat itaque in velit saepe quis eaque
+        voluptatum?
+      </p>
     </div>
   </div>
 </template>
@@ -48,8 +126,45 @@ export default {
 </script>
 
 <style scoped>
+#h3 {
+  width: 100%;
+  justify-content: flex-end;
+  text-align: right;
+  font-size: 15px;
+  cursor: pointer;
+}
+
+.card-text:hover {
+  color: #0c9cda;
+  font-weight: bold;
+  transition: 0.3s;
+}
+.card-body p {
+  color: #007bff;
+  border: 1px solid #007bff;
+  border-radius: 15px;
+}
+button {
+  width: 80%;
+}
+
+button:hover {
+  background-color: white !important;
+  color: #0c9cda;
+  border: 1px solid #0c9cda;
+}
+
 button span {
   font-weight: 500;
+}
+.card-services {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.card-services span {
+  font-size: 25px;
+  font-weight: bold;
 }
 #text-content {
   width: 50%;
@@ -57,14 +172,20 @@ button span {
   flex-direction: column;
   align-content: space-around;
 }
+
 h1 {
   color: #0c9cda;
   font-weight: bold;
 }
+
 h2 {
   color: #707070;
   width: 50%;
   font-weight: 100;
+}
+#services {
+  display: flex;
+  justify-content: space-evenly;
 }
 .container {
   align-self: center;
@@ -76,33 +197,69 @@ h2 {
   text-align: left;
   height: 100% !important;
 }
+
+.card-text:hover {
+  color: white;
+  border: 1px solid white;
+  background-color: #0c9cda;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.span-product {
+  font-size: 15px;
+}
+
 .clothes {
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
 }
+
+.cloth span {
+  justify-self: center;
+  color: #0c9cda;
+}
+
+.cloth span {
+  text-align: center;
+  border: 1px solid #0c9cda;
+  border-radius: 12px;
+  width: 50%;
+  font-weight: bold;
+}
+
 .clothes img {
   border-radius: 5px;
+  border: 1px solid #0c9cda;
 }
+
 .container img {
-  width: 100%;
-  height: 100%;
+  height: 70%;
 }
 
 .shop-now-button:hover {
   background-color: #007bff;
 }
-
+.card-title {
+  font-weight: bold;
+}
 .img-container {
   width: 40%;
   height: 50%;
 }
+
 /* New CSS styles */
 #prodcut-img {
   display: flex;
   justify-content: space-between;
   width: 100% !important;
 }
+
+#prodcut-img .card {
+  width: 20% !important;
+}
+
 .success-starts img {
   width: 250px;
 }
@@ -114,8 +271,9 @@ h2 {
   flex-direction: column;
   width: 50%;
 }
+
 .shop-now-button {
-  width: 150px;
+  width: 140px;
   height: 30px;
   padding: 5px 18px 30px;
   border: 1px solid white;
@@ -123,10 +281,13 @@ h2 {
   color: white;
   background-color: #007bff;
   margin-bottom: 50px;
+  font-weight: bold;
 }
+
 .success-starts {
   display: flex;
 }
+
 .shop-now-button img {
   width: 25px;
   height: 25px;
@@ -135,5 +296,10 @@ h2 {
 .success-starts p {
   flex: 1;
   margin-left: 10px;
+}
+@media (max-width: 1250px) {
+  .clothes-card {
+    display: block;
+  }
 }
 </style>
--- src/components/ProductListComponent.vue
+++ src/components/ProductListComponent.vue
@@ -3,17 +3,20 @@
     <h2>Product List</h2>
   </div>
   <div class="container">
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-      <div class="col" v-for="product in products" :key="product.name">
-        <div class="card">
-          <img :src="product.imageUrl" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">{{ product.name }}</h5>
-            <p class="card-text">{{ product.description }}</p>
-            <p class="card-text">{{ product.price }}$</p>
-            <button class="btn btn-primary" @click="addToCart(product)">
-              Buy
-            </button>
+    <div class="container shadow p-3 mb-5 bg-body rounded" id="cloathing">
+      <div class="row row-cols-1 row-cols-md-2 g-4 mb-5 mt-5 clothes-card">
+        <div class="col" v-for="product in products" :key="product.name">
+          <div class="card" style="width: 12rem">
+            <img :src="product.imageUrl" class="card-img-top" alt="..." />
+            <div class="card-body text-center">
+              <h5 class="card-title">{{ product.name }}</h5>
+              <p class="card-text">
+                <span class="span-product mt-2">{{ product.price }} TND</span>
+              </p>
+              <button class="btn btn-primary" @click="addToCart(product)">
+                Buy
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -42,6 +45,30 @@ export default {
           price: 65,
           imageUrl: require("../assets/t-shirt.png"),
         },
+        {
+          name: "T-shirt Super Mario",
+          description: "T-shirt avec la photo de Mario",
+          price: 50,
+          imageUrl: require("../assets/t-shirt.png"),
+        },
+        {
+          name: "T-shirt Super Mario",
+          description: "T-shirt avec la photo de Mario",
+          price: 50,
+          imageUrl: require("../assets/t-shirt.png"),
+        },
+        {
+          name: "T-shirt Super Mario",
+          description: "T-shirt avec la photo de Mario",
+          price: 50,
+          imageUrl: require("../assets/t-shirt.png"),
+        },
+        {
+          name: "T-shirt Super Mario",
+          description: "T-shirt avec la photo de Mario",
+          price: 50,
+          imageUrl: require("../assets/t-shirt.png"),
+        },
       ],
       cart: [],
     };
@@ -63,14 +90,71 @@ export default {
 </script>
 
 <style scoped>
-img {
-  height: 120px;
-  width: 120px;
+/* Special CSS for the provided HTML structure */
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
 }
 
-.cart {
-  background-color: #f8d7da;
-  padding: 10px;
-  margin-top: 20px;
+.row-cols-md-2 {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+
+.col {
+  flex: 0 0 20%; /* Adjust as needed */
+}
+
+.card {
+  width: 100%;
+  border: 1px solid #0c9cda;
+  border-radius: 5px;
+  overflow: hidden;
+  transition: transform 0.3s ease-in-out;
+}
+
+.card:hover {
+  transform: scale(1.05);
+}
+
+.card img {
+  width: 100%;
+  height: auto;
+  border-radius: 5px;
+}
+
+.card-body {
+  padding: 15px;
+}
+
+.card-title {
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.card-text {
+  text-align: center;
+}
+
+.span-product {
+  font-size: 15px;
+  font-weight: bold;
+}
+
+.btn-primary {
+  width: 100%;
+  background-color: #007bff;
+  color: white;
+  border: 1px solid #007bff;
+  border-radius: 5px;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.btn-primary:hover {
+  background-color: #0056b3;
 }
 </style>
